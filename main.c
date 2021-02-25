360a32b83a09369e518b7a81ec3530a7bd1ec25a
OanaVasilescu <vasilescu.oana28@gmail.com>
2021-02-25 17:43:26 +0200

    incercare

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  PAD_chat2
+//
+//  Created by Oana Vasilescu on 25/02/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
